191d77e2eb8f0cdc3d26e8ced9df357c11cf389f
Philimon Derib <58902914+philimon-reset@users.noreply.github.com>
2021-12-10 00:00:08 +0300

    Update 11-need_meeting.sql

--- 0x00-MySQL_Advanced/11-need_meeting.sql
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -4,4 +4,4 @@ CREATE VIEW need_meeting
   AS
   SELECT name
   FROM students
-  WHERE (score <= 80) AND ((last_meeting IS NULL) OR (MONTH(NOW()) - MONTH(last_meeting) < 1));
+  WHERE (score < 80) AND ((last_meeting IS NULL) OR (MONTH(NOW()) - MONTH(last_meeting) > 1));
