f7543898a1a93a3336ec5659a8fa656ad1651e99
philimon-reset <philimonderib2019@gmail.com>
2021-12-09 16:46:40 +0300

    test

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,6 +1,6 @@
 -- query and list all bands with Glam rock as their main style, ranked by their longevity
 
-SELECT band_name, IF(split is NULL , year(curdate()) - formed, (split - formed))  AS 'lifespan'
+SELECT band_name, IF(split is NULL , 2020 - formed, (split - formed))  lifespan
 FROM metal_bands
-WHERE `style` LIKE '%Glam rock%'
+WHERE style LIKE '%Glam rock%'
 ORDER BY lifespan DESC;
