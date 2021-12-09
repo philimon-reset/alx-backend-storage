8217c4eda860ce611ea845f5a9660220a4c0c0bb
philimon-reset <philimonderib2019@gmail.com>
2021-12-09 17:39:18 +0300

    do 10 and 11 #

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -4,8 +4,8 @@ DELIMITER //
 CREATE TRIGGER chang BEFORE UPDATE ON users
 FOR EACH ROW
 BEGIN
-IF NEW.email <=> OLD.email THEN
-SET NEW.valid_email = 0
+IF NEW.email != OLD.email THEN
+SET NEW.valid_email = 0;
 END IF;
 END //
 DELIMITER ;
