afa9d9b2c503c271eb65c179fd3b85ee5dc2d104
philimon-reset <philimonderib2019@gmail.com>
2021-12-09 17:17:13 +0300

    do 10 and 11 #

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,11 +1,18 @@
 -- computes and store the average score for a student.
 
 DELIMITER //
+DELIMITER //
 CREATE
-PROCEDURE ComputeAverageScoreForUser (IN user_id INT)
-BEGIN
-DECLARE stud FLOAT;
-SET stud_avg = (SELECT AVG(score) FROM corrections WHERE corrections.user_id = user_id);
-UPDATE users SET average_score = stud WHERE users.id = user_id;
-END //
+  PROCEDURE ComputeAverageScoreForUser (IN user_id INT)
+  BEGIN
+    DECLARE
+      stud_avg FLOAT;
+    SET stud_avg = (SELECT
+                    AVG(score)
+                    FROM corrections
+                    WHERE corrections.user_id = user_id);
+    UPDATE users
+    SET average_score = stud_avg
+    WHERE users.id = user_id;
+  END //
 DELIMITER ;
