07ff428f55c3e40ee954a399d3f02891fe265a3e
philimon-reset <philimonderib2019@gmail.com>
2021-12-09 17:43:57 +0300

    do 10 and 11 #

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -8,7 +8,7 @@ SET try1 = (SELECT project_name IN (SELECT name FROM projects));
 IF try1 = 0 THEN
 	INSERT INTO projects(name) VALUES(project_name);
 END IF;
-SET p_id = (SELECT id FROM projects WHERE name = project_name LIMIT 1);
+SET p_id = (SELECT id FROM projects WHERE name = project_name);
 INSERT INTO corrections(user_id, project_id, score) VALUE(user_id, p_id, score);
 END //
 DELIMITER ;
