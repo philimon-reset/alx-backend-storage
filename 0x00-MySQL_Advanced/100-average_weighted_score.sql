a1a67fa0cd734bdb153387d47b33a36fd2b68b42
philimon-reset <philimonderib2019@gmail.com>
2021-12-11 07:49:00 +0300

    mysql first advanced #

--- 0x00-MySQL_Advanced/100-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -7,7 +7,7 @@ DECLARE total_W, final_W INT;
 SET total_W = ((SELECT SUM(`weight`) FROM projects WHERE id IN (SELECT
       project_id
       FROM corrections
-      WHERE corrections.user_id = user_id)););
+      WHERE corrections.user_id = user_id)));
 SET final_W = (SELECT SUM(projects.weight * corrections.score) FROM projects
 INNER JOIN corrections ON corrections.project_id = projects.id AND corrections.user_id = user_id);
 UPDATE users SET users.average_score = final_W / total_W WHERE users.id = user_id;
