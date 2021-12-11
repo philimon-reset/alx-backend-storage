e45107ded15696dc74b0a43cd1ca6f37be7dff5a
philimon-reset <philimonderib2019@gmail.com>
2021-12-11 16:59:46 +0300

    bad move but lets try it anyway #

--- 0x00-MySQL_Advanced/101-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/101-average_weighted_score.sql
@@ -3,10 +3,16 @@
 DELIMITER //
 CREATE PROCEDURE `ComputeAverageWeightedScoreForUsers` ()
 BEGIN
-DECLARE total_W, final_W INT;
+DECLARE total_W, final_W, check_C INT;
+DECLARE check_A INT DEFAULT 1;
 SET total_W = ((SELECT SUM(`weight`) FROM projects));
-SET final_W = (SELECT SUM(projects.weight * corrections.score) FROM projects
-INNER JOIN corrections ON corrections.project_id = projects.id AND corrections.user_id = user_id);
-UPDATE users SET users.average_score = final_W / total_W WHERE users.id = user_id;
+SET check_C = (SELECT COUNT(id) FROM users);
+
+WHILE check_A < check_C + 1 DO
+    SET final_W = (SELECT SUM(projects.weight * corrections.score) FROM projects
+    INNER JOIN corrections ON corrections.project_id = projects.id AND corrections.user_id = check_A);
+    UPDATE users SET users.average_score = final_W / total_W WHERE users.id = check_A;
+    SET check_A = check_A + 1;
+    END WHILE;
 END //
 DELIMITER ;
