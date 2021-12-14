536c6d7b5023a30921bbb3ff05f16e52ca8ec555
philimon-reset <philimonderib2019@gmail.com>
2021-12-14 12:30:01 +0300

    very bad solution

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -1,1 +1,8 @@
 #!/usr/bin/env python3
+""" returns all students sorted by average score """
+
+import pymongo
+
+
+def top_students(mongo_collection):
+    """ main function """
--- 0x01-NoSQL/102-log_stats.py
+++ 0x01-NoSQL/102-log_stats.py
@@ -1,1 +1,28 @@
 #!/usr/bin/env python3
+""" nginx logs to mongo instance parser """
+
+from pymongo import MongoClient
+
+
+if __name__ == "__main__":
+    """ main process """
+    client = MongoClient()
+    db = client.logs
+    coll = [x for x in db.nginx.find()]
+    ip = [x["ip"] for x in db.nginx.aggregate([{"$project": {"_id":0, "ip" : 1}}])]
+    method_C = {"GET": 0, "POST": 0, "PUT": 0, "PATCH": 0, "DELETE": 0}
+    status = 0
+    ip_T = {i : ip.count(i) for i in ip}
+    for i in coll:
+        if i["method"] in method_C:
+            method_C[i["method"]] += 1
+        if i["method"] == "GET" and i["path"] == "/status":
+            status += 1
+    print(f"{len(coll)} logs")
+    print("Methods:")
+    for key, value in method_C.items():
+        print(f"\tmethod {key}: {value}")
+    print(f"{status} status check", end="")
+    print("IPs:")
+    for key, value in ip_T.items():
+        print(f"\t{key}: {value}")
