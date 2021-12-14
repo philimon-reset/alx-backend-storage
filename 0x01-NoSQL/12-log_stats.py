b2930afe5a7920d2a6b73c66289cf279690bfc9a
philimon-reset <philimonderib2019@gmail.com>
2021-12-14 10:41:43 +0300

    mandatory done

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,1 +1,21 @@
 #!/usr/bin/env python3
+""" nginx logs to mongo instance parser """
+
+from pymongo import MongoClient
+
+
+if __name__ == "__main__":
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    db = client.logs
+    coll = db.nginx
+    method_C = {"GET": 0, "POST": 0, "PUT" : 0, "PATCH" : 0, "DELETE" : 0}
+    status = 0
+    for i in coll.find():
+        method_C[i["method"]] += 1
+        if i["method"] == "GET" and i["path"] == "/status":
+            status += 1
+    print(f"{len(coll)} logs")
+    print("Methods:")
+    for key, value in method_C.items():
+        print(f"\tmethod {key}: {value}")
+    print(f"{status} status check")
