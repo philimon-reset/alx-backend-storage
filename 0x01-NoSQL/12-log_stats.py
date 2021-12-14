52ac70c665b7ac7fefdd7412eada2156b0049651
philimon-reset <philimonderib2019@gmail.com>
2021-12-14 11:14:19 +0300

    pep

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -4,18 +4,19 @@
 from pymongo import MongoClient
 
 
-client = MongoClient('mongodb://127.0.0.1:27017')
-db = client.logs
-coll = [x for x in db.nginx.find()]
-method_C = {"GET": 0, "POST": 0, "PUT": 0, "PATCH": 0, "DELETE": 0}
-status = 0
-for i in coll:
-    if i["method"] in method_C:
-        method_C[i["method"]] += 1
-    if i["method"] == "GET" and i["path"] == "/status":
-        status += 1
-print(f"{len(coll)} logs")
-print("Methods:")
-for key, value in method_C.items():
-    print(f"\tmethod {key}: {value}")
-print(f"{status} status check")
+if __name__ == "__main__":
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    db = client.logs
+    coll = [x for x in db.nginx.find()]
+    method_C = {"GET": 0, "POST": 0, "PUT": 0, "PATCH": 0, "DELETE": 0}
+    status = 0
+    for i in coll:
+        if i["method"] in method_C:
+            method_C[i["method"]] += 1
+        if i["method"] == "GET" and i["path"] == "/status":
+            status += 1
+    print(f"{len(coll)} logs")
+    print("Methods:")
+    for key, value in method_C.items():
+        print(f"\tmethod {key}: {value}")
+    print(f"{status} status check")
