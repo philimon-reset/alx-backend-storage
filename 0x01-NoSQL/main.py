a63d6bc27c81813cddc5413c150e48a0d7f1552d
philimon-reset <philimonderib2019@gmail.com>
2021-12-14 09:20:40 +0300

    6 and 7

--- 0x01-NoSQL/main.py
+++ 0x01-NoSQL/main.py
@@ -2,7 +2,6 @@
 """ 8-main """
 from pymongo import MongoClient
 list_all = __import__('8-all').list_all
-
 if __name__ == "__main__":
     client = MongoClient('mongodb://127.0.0.1:27017')
     school_collection = client.my_db.school
