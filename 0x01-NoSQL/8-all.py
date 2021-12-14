70c97f5bb8c00fb3f667c8b17a11ac14fc82605f
philimon-reset <philimonderib2019@gmail.com>
2021-12-14 09:03:24 +0300

    8 test

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,2 +1,9 @@
 #!/usr/bin/env python3
+""" list all documents using pymongo"""
+
 import pymongo
+
+def list_all(mongo_collection):
+    """ main function to list documents"""
+    main = [x for x in mongo_collection.find({})]
+    return main
