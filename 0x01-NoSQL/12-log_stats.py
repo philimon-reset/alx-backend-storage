1abc0da62d2035114fbc7e062fabe5c14f52e57f
philimon-reset <philimonderib2019@gmail.com>
2021-12-14 10:43:03 +0300

    appended to advanced

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -7,11 +7,12 @@ from pymongo import MongoClient
 if __name__ == "__main__":
     client = MongoClient('mongodb://127.0.0.1:27017')
     db = client.logs
-    coll = db.nginx
-    method_C = {"GET": 0, "POST": 0, "PUT" : 0, "PATCH" : 0, "DELETE" : 0}
+    coll = [x for x in db.nginx.find()]
+    method_C = {"GET": 0, "POST": 0, "PUT": 0, "PATCH": 0, "DELETE": 0}
     status = 0
-    for i in coll.find():
-        method_C[i["method"]] += 1
+    for i in coll:
+        if i["method"] in method_C:
+            method_C[i["method"]] += 1
         if i["method"] == "GET" and i["path"] == "/status":
             status += 1
     print(f"{len(coll)} logs")
--- 0x01-NoSQL/main.py
+++ 0x01-NoSQL/main.py
@@ -1,22 +1,24 @@
 #!/usr/bin/env python3
-""" nginx logs to mongo instance parser """
-
+""" 11-main """
 from pymongo import MongoClient
-
+list_all = __import__('8-all').list_all
+insert_school = __import__('9-insert_school').insert_school
+schools_by_topic = __import__('11-schools_by_topic').schools_by_topic
 
 if __name__ == "__main__":
     client = MongoClient('mongodb://127.0.0.1:27017')
-    db = client.logs
-    coll = [x for x in db.nginx.find()]
-    method_C = {"GET": 0, "POST": 0, "PUT" : 0, "PATCH" : 0, "DELETE" : 0}
-    status = 0
-    for i in coll:
-        if i["method"] in method_C:
-            method_C[i["method"]] += 1
-        if i["method"] == "GET" and i["path"] == "/status":
-            status += 1
-    print(f"{len(coll)} logs")
-    print("Methods:")
-    for key, value in method_C.items():
-        print(f"\tmethod {key}: {value}")
-    print(f"{status} status check")
+    school_collection = client.my_db.school
+
+    j_schools = [
+        { 'name': "Holberton school", 'topics': ["Algo", "C", "Python", "React"]},
+        { 'name': "UCSF", 'topics': ["Algo", "MongoDB"]},
+        { 'name': "UCLA", 'topics': ["C", "Python"]},
+        { 'name': "UCSD", 'topics': ["Cassandra"]},
+        { 'name': "Stanford", 'topics': ["C", "React", "Javascript"]}
+    ]
+    for j_school in j_schools:
+        insert_school(school_collection, **j_school)
+
+    schools = schools_by_topic(school_collection, "Python")
+    for school in schools:
+        print("[{}] {} {}".format(school.get('_id'), school.get('name'), school.get('topics', "")))
