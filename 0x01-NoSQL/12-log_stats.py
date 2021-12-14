3a054fcfb45ac20c2866ee99281c62eb0e7c84b4
philimon-reset <philimonderib2019@gmail.com>
2021-12-14 17:00:31 +0300

    test

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -5,17 +5,18 @@ from pymongo import MongoClient
 
 
 def temp_F():
-	""" temp function to check the checker"""
-	client = MongoClient()
-	main = client.logs.nginx
-	method_C = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-	status = main.count_documents({"method": "GET", "path": "/status"})
-	print(f"{main.estimated_document_count()} logs")
-	print("Methods:")
-	for i in method_C:
-		check = main.count_documents({"method": i})
-		print(f"\tmethod {i}: {check}")
-	print(f"{status} status check")
+    """ temp function to check the checker"""
+    client = MongoClient()
+    main = client.logs.nginx
+    method_C = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    status = main.count_documents({"method": "GET", "path": "/status"})
+    print(f"{main.estimated_document_count()} logs")
+    print("Methods:")
+    for i in method_C:
+        check = main.count_documents({"method": i})
+        print(f"\tmethod {i}: {check}")
+    print(f"{status} status check")
+
 
 if __name__ == "__main__":
-	temp_F()
+    temp_F()
