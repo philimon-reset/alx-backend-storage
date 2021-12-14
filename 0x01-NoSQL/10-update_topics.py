4d57e86a0cb6ea9da88e63d9014d64b1aa74716e
philimon-reset <philimonderib2019@gmail.com>
2021-12-14 09:40:39 +0300

    9

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -1,1 +1,9 @@
 #!/usr/bin/env python3
+"""  changes all topics of a school document based on the name """
+
+import pymongo
+
+
+def update_topics(mongo_collection, name, topics):
+    """ main function """
+    return mongo_collection.insert_one(kwargs).inserted_id
